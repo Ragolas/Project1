9f1b0c4233c2a1a3c083263b3b844a35074f3e3c
PAVLO <PAVLO>
2020-04-08 13:47:45 -0500

    My first commit

---
+++ POOFACe/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  POOFACe
+//
+//  Created by Pablo Ruiz-Velasco on 4/7/20.
+//  Copyright © 2020 JOE MAMA. All rights reserved.
+//
+
+import UIKit
+class ViewController: UIViewController {
+
+    @IBOutlet weak var NAPER: UILabel!
+    
+    var tapcount = 0
+    
+    @IBAction func BUTTONPRESS(_ sender: Any) {
+    tapcount = tapcount + 1
+        
+        if tapcount >= 10 {
+            NAPER.text = "you have pushed the button ten times"
+        }
+    }
+    
+    @IBOutlet weak var LABEL: UILabel! 
+    
+    @IBAction func COLOR(_ sender: Any) {
+        self.view.backgroundColor = UIColor.red
+    }
+    
+    
+    
+    @IBAction func COOL(_ sender: Any) {
+        NAPER.text = "BUTTONS ARE COOL"
+        print("button tapped")
+    }
+    
+    @IBAction func GREEN(_ sender: Any) {
+        self.view.backgroundColor = UIColor.red
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    
+}
+}
